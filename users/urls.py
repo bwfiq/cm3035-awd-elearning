e8ef041247b4ff54af1c1e6ffeaa63d4d9968bbb
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-02-04 13:31:44 +0800

    feat: create templates with base template

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,7 @@ from users import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
     path("users/<name>", views.hello_there, name='hello_there'),
 ]
--- users/views.py
+++ users/views.py
@@ -4,7 +4,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, "users/home.html")
+
+def about(request):
+    return render(request, "users/about.html")
+
+def contact(request):
+    return render(request, "users/contact.html")
 
 def hello_there(request, name):
     # now = datetime.now()

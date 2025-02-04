978975a04b0f8687a3d195ad82920c1e3588d34e
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-02-04 12:55:38 +0800

    feat: add static css file for styling

--- elearning_app/settings.py
+++ elearning_app/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -117,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static_collected'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
